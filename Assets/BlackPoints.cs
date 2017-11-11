29c65b126f46db100ec116eff02830c4d98d8b28
dwayne valdelievre <dwayne.valdelievre@gmail.com>
2017-11-11 23:20:14 +0000

    added script for counting score

---
+++ Assets/BlackPoints.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackPoints : MonoBehaviour
+{
+
+    int points = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<TextMesh>().text = points.ToString();
+    }
+
+    public void AddPointsForBlack(int piece)
+    {
+        switch (piece)
+        {
+            case 1:
+                points = +5;
+                break;
+            case 2:
+                points = +3;
+                break;
+            case 3:
+                points = +3;
+                break;
+            case 4:
+                points = +9;
+                break;
+            case 6:
+                points = +1;
+                break;
+        }
+    }
+}
